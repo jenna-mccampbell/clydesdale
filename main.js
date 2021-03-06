3efd257d6f49b112197cd752e13b6acb55bface6
jenna-mccampbell <jenna.mccampbell@comcast.net>
2021-03-06 12:54:21 -0700

    compute fabric based on user interaction

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
+const zip = (a, b) => a.map((k, i) => [k, b[i]]);
+
 let box_size = 20;
 let grid;
+let warp_ends = 20;
+let shafts = 4;
+let treadles = 6;
+let pat_length = 20;
+let state = {
+  treadling: createArray(treadles, pat_length, false),
+  threading: createArray(warp_ends, shafts, false),
+  tie_up: createArray(treadles, shafts, false),
+};
 
 function init() {
   grid = d3
@@ -13,14 +24,19 @@ function init() {
 window.onresize = function () {};
 
 function draw() {
+  d3.select("svg").selectAll("*").remove();
   let margin = 20;
-  let warp_ends = 20;
-  let shafts = 4;
-  let treadles = 6;
-  let pat_length = 20;
 
   // grid for threading
-  drawGrid(margin, margin, shafts, warp_ends, "threading");
+  drawGrid(
+    margin,
+    margin,
+    shafts,
+    warp_ends,
+    "threading",
+    true,
+    state.threading
+  );
 
   // grid for tie up
   drawGrid(
@@ -28,7 +44,9 @@ function draw() {
     margin,
     shafts,
     treadles,
-    "tieup"
+    "tieup",
+    true,
+    state.tie_up
   );
 
   //grid for treadling
@@ -37,8 +55,13 @@ function draw() {
     margin + box_size * shafts + margin,
     pat_length,
     treadles,
-    "treadling"
+    "treadling",
+    true,
+    state.treadling
   );
+
+  fabric = weave(state.treadling, state.threading, state.tie_up);
+  console.log(fabric);
 
   //grid for fabric
   drawGrid(
@@ -46,7 +69,9 @@ function draw() {
     margin + box_size * shafts + margin,
     pat_length,
     warp_ends,
-    "fabric"
+    "fabric",
+    false,
+    fabric
   );
 }
 
@@ -54,7 +79,8 @@ function getRectColor(row_index, col_index) {
   return "#FFFFFF";
 }
 
-function drawGrid(start_x, start_y, row_num, col_num, name) {
+function drawGrid(start_x, start_y, row_num, col_num, name, clickable, arr) {
+  console.log(name);
   let data = new Array();
   for (let row_index = 0; row_index < row_num; row_index++) {
     data.push(new Array());
@@ -66,7 +92,8 @@ function drawGrid(start_x, start_y, row_num, col_num, name) {
         y: rect_y,
         width: box_size,
         height: box_size,
-        click: 0,
+        row_index: row_index,
+        col_index: col_index,
       });
     }
   }
@@ -98,25 +125,54 @@ function drawGrid(start_x, start_y, row_num, col_num, name) {
     .attr("height", function (d) {
       return d.height;
     })
-    .style("fill", "#fff")
+    .style("fill", function (d) {
+      if (arr) {
+        console.log(name, arr[d.row_index][d.col_index]);
+        if (arr[d.row_index][d.col_index]) {
+          return "#838690";
+        } else {
+          return "#fff";
+        }
+      }
+    })
     .style("stroke", "#222")
     .on("click", function (d) {
-      d.click++;
-      if (d.click % 4 == 0) {
-        d3.select(this).style("fill", "#fff");
-      }
-      if (d.click % 4 == 1) {
-        d3.select(this).style("fill", "#2C93E8");
-      }
-      if (d.click % 4 == 2) {
-        d3.select(this).style("fill", "#F56C4E");
-      }
-      if (d.click % 4 == 3) {
-        d3.select(this).style("fill", "#838690");
+      if (clickable) {
+        arr[d.row_index][d.col_index] ^= true;
+        draw();
       }
     });
 }
 
 function handleUserInput(a) {
   console.log(a);
+}
+
+function createArray(x, y, item) {
+  let arr = new Array(x);
+  for (let i = 0; i < y; i++) {
+    arr[i] = new Array(y);
+    arr[i].fill(item);
+  }
+  return arr;
+}
+
+function weave(treadling, threading, tie_up) {
+  fabric = createArray(threading[0].length, treadling.length, false);
+  for (let i = 0; i < fabric.length; i++) {
+    for (let j = 0; j < fabric[0].length; j++) {
+      threading_state = threading.map((row) => row[j]);
+      treadling_state = treadling[i];
+      thread_shaft = threading_state.findIndex((element) => element);
+      if (thread_shaft == -1) {
+        fabric_state = false;
+      } else {
+        fabric_state = zip(tie_up[thread_shaft], treadling_state).some(
+          (arr) => arr[0] && arr[1]
+        );
+      }
+      fabric[i][j] = fabric_state;
+    }
+  }
+  return fabric;
 }
